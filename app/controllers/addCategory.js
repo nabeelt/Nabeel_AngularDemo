08db9dca3163276783c62336ef182e6bbb59f5ef
nabeel <nabeel@qburst.com>
2015-08-21 16:52:09 +0530

    drag and drop added

--- app/controllers/addCategory.js
+++ app/controllers/addCategory.js
@@ -1,26 +1,32 @@
 app.controller("addCategory",['$scope','$localStorage',function ($scope,$localStorage) {
-		$scope.categoryJSON =[];
 		$scope.activity;
 		if($localStorage.categoryJSON){
 			$scope.categoryJSON = $localStorage.categoryJSON;
 		}
 		$scope.addNewCategory = function(){
-			$scope.categoryJSON.push({
-					category : $scope.category,
+			if($scope.new_category){
+				$scope.categoryJSON.push({
+					category : $scope.new_category,
 					activities : []
 				});
 			$localStorage.categoryJSON = $scope.categoryJSON;
+			}	
 		}
 		$scope.removeCategory = function(index){
 			$scope.categoryJSON.splice(index,1);
 		}
 		$scope.addNewActivity = function(index,activity){
-			$scope.categoryJSON[index].activities.push(activity);
-			$localStorage.categoryJSON = $scope.categoryJSON;
+			if(activity){
+				$scope.categoryJSON[index].activities.push(activity);
+				$localStorage.categoryJSON = $scope.categoryJSON;
+			}
 		}
 		$scope.removeActivity = function(index,parent){
-			debugger;
 			$scope.categoryJSON[parent].activities.splice(index,1);
+		}
+		$scope.todoSortable = {
+			cursor : "move",
+			tolerance : "intersect"
 		}
 	}]);
 
---
+++ app/services/app.storejson.js
@@ -0,0 +1,8 @@
+// app.service('newService',['$http',
+// 	function($http){
+// 		var categoryJSON =[];
+// 		this.getData = function(){
+// 			return $http.get(categoryJSON);
+// 		}
+	
+// }]);
